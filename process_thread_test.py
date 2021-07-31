77ecdd7a1b81a38f09a37b27ecbcd1d9cac3b36d
minano430 <34651757+minano430@users.noreply.github.com>
2021-07-31 13:43:47 +0800

    IO commit

--- process_thread_test.py
+++ process_thread_test.py
@@ -4,6 +4,8 @@ import time
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+import string
 
 
 def multithreading(func, args, workers):
@@ -27,6 +29,13 @@ def cpu_heavy(x):
     stop = time.time()
     return start, stop
 
+def io_heavy(text):
+    start = time.time()
+    f = open('output.txt', 'wt', encoding='utf-8')
+    f.write(text)
+    f.close()
+    stop = time.time()
+    return start,stop
 
 def visualize_runtimes(results, title):
     start, stop = np.array(results).T
@@ -34,15 +43,19 @@ def visualize_runtimes(results, title):
     plt.grid(axis='x')
     plt.ylabel("Tasks")
     plt.xlabel("Seconds")
-    plt.xlim(0, 22.5)
+    plt.xlim(0, 0.1)
     ytks = range(len(results))
     plt.yticks(ytks, ['job {}'.format(exp) for exp in ytks])
     plt.title(title)
     return stop[-1] - start[0]
 
 if __name__ == '__main__': 
+    N=12
+    TEXT = ''.join(random.choice(string.ascii_lowercase) for i in range(10**7*5))
     plt.subplot(1, 2, 1)
-    visualize_runtimes(multithreading(cpu_heavy, range(4), 4), "Multithreading")
+    # visualize_runtimes(multithreading(cpu_heavy, range(4), 4), "Multithreading")
+    visualize_runtimes(multithreading(io_heavy, [TEXT for i in range(N)], 1),"Single Thread")
     plt.subplot(1, 2, 2)
-    visualize_runtimes(multiprocessing(cpu_heavy, range(4), 4), "Multiprocessing")
+    # visualize_runtimes(multiprocessing(cpu_heavy, range(4), 4), "Multiprocessing")
+    visualize_runtimes(multiprocessing(io_heavy, [TEXT for i in range(N)], 1),"Single Process")
     plt.show()
